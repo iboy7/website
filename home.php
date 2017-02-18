456cb66f5d5f3b69b6af043673b42ffd7f6443e8
Marcin Kaszkowiak <marcinkaszkowiak@pc2.home>
2017-02-19 00:32:08 +0100

    quotes

--- home.php
+++ home.php
@@ -8,5 +8,14 @@
             Welcome to the official webpage of the Students' Scientific Society of Genetics and Molecular Medicine.
         </p>
 
+        <p class="quote" >
+            "Look deep into nature, and then you will understand everything better."
+        </p>
+
+        <p class="signature" >
+            Albert Einstein
+        </p>
+
+
     </body>
 </html>
